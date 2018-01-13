c144f32fe43fb3831dd3d2898b9bcaffcd1764dc
talend <talend@localhost.localdomain>
2018-01-13 06:49:01 -0500

    More logs and keep process active in entrypoint

--- assets/endpoint.sybase.iq.sh
+++ assets/endpoint.sybase.iq.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 # start server and then create a database
 source /opt/sybase/iq16/SYBASE.sh
-export PATH=$PATH:/opt/sybase/iq16/IQ-16_0/bin64/
-cd /var/sybase/IQ1
+#export PATH=$PATH:/opt/sybase/iq16/IQ-16_0/bin64/
+#cd /var/sybase/IQ1
 start_iq @./params.cfg ./IQ1.db
+
+while true; do sleep 10000; done
